f3b27033764ddbdf9d0696e42616183814aea1ff
Glaucio Daniel <glauciodaniel@gmail.com>
2022-05-26 09:20:47 -0300

    Deploy

--- src/dto/updateHello.dto.ts
+++ src/dto/updateHello.dto.ts
@@ -1,7 +1,8 @@
+// 1º instale o comando abaixo:
+// npm install @nestjs/mapped-types
+
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateHelloDTO } from './createHello.dto';
-
-// npm install @nestjs/mapped-types
 
 export class UpdateHelloDTO extends PartialType(
   OmitType(CreateHelloDTO, [] as const),
